328881879a6779e11cc3db01d06f9b381c5260cc
Ruben van Eldik <rubenvaneldik@protonmail.com>
2021-03-08 11:38:26 +0100

    fix: Use baseURL instead of base_url

--- plugin.js
+++ plugin.js
@@ -2,7 +2,7 @@ import { createClient } from 'nhost-js-sdk'
 
 export default function (context, inject) {
   const options = {
-    base_url: '<%= options.base_url %>',
+    baseURL: '<%= options.base_url %>',
     use_cookies: '<%= options.use_cookies %>' === 'true',
     refresh_interval_time: +'<%= options.refresh_interval_time %>' || null,
     client_storage_type: '<%=options.client_storage_type %>' || undefined
